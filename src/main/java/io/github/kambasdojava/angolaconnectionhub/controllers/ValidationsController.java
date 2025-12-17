9837279f0ff8bd6c7c886f481e584e006fee947a
Inocencio Cordeiro Armando <34503843+InocencioC@users.noreply.github.com>
2025-12-17 20:46:00 +0000

    feat: nif response.

--- src/main/java/io/github/kambasdojava/angolaconnectionhub/controllers/ValidationsController.java
+++ src/main/java/io/github/kambasdojava/angolaconnectionhub/controllers/ValidationsController.java
@@ -1,12 +1,16 @@
 package io.github.kambasdojava.angolaconnectionhub.controllers;
 
+import io.github.kambasdojava.angolaconnectionhub.dto.TaxData;
+import io.github.kambasdojava.angolaconnectionhub.dto.TaxDataRequest;
 import io.github.kambasdojava.angolaconnectionhub.dto.ValidateTaxIdRequest;
 import io.github.kambasdojava.angolaconnectionhub.dto.ValidateTaxIdResponse;
+import io.github.kambasdojava.angolaconnectionhub.services.ValidationsService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ValidationsController {
 
+  private final ValidationsService validationsService;
+
   @PostMapping("/tax-id")
-  public ResponseEntity<@NonNull ValidateTaxIdResponse> validateTaxId(@Valid ValidateTaxIdRequest request) {
-    return null;
+  public ResponseEntity<@NonNull ValidateTaxIdResponse> validateTaxId(@Valid @RequestBody ValidateTaxIdRequest request) {
+    TaxDataRequest taxDataRequest = new TaxDataRequest(request.taxId());
+    TaxData taxData = validationsService.getTaxData(taxDataRequest);
+    ValidateTaxIdResponse response = new ValidateTaxIdResponse(
+            taxData.name(),
+            taxData.taxId(),
+            taxData.type(),
+            taxData.isActive()
+    );
+    return ResponseEntity.ok(response);
   }
 }
